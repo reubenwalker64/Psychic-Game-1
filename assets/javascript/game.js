e5385fdd806bc00fefbab3c87ffbae9d1081991c
reubenwalker64 <rwalker@mobile-atom.com>
2018-12-11 10:52:02 -0500

    1st commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+/* The Psycic game
+Guess what letter I'm thinking of
+Wins: (# of times the user has guessed the letter correctly)
+Losses: (# of times the user has failed to guess the letter correctly after exhausting all guesses)
+Guesses left: (# of guesses left. This will update)
+Your Guesses so far: (the specific letters that the user typed. Display these until the user either wins or loses.)
+Note: When the player wins, increase the Wins counter and start the game over again (without refreshing the page).
+Note: When the player loses, increase the Losses counter and restart the game without a page refresh (just like when the user wins).
+*/
