6770a6f483158dbcf19e5787f39db28b64e09121
reubenwalker64 <rwalker@mobile-atom.com>
2018-12-12 18:17:38 -0500

    3rd commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,3 +7,91 @@ Your Guesses so far: (the specific letters that the user typed. Display these un
 Note: When the player wins, increase the Wins counter and start the game over again (without refreshing the page).
 Note: When the player loses, increase the Losses counter and restart the game without a page refresh (just like when the user wins).
 */
+
+// GUESS WHAT LETTER - Varible or array listing alphabet
+
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var arrayLength = alphabet.length;
+
+console.log(aphabet.length);
+
+// Creating variables to hold the number of wins, losses, guesses left, and guess so far. They start at 0.
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 0;
+var guessesSoFar = 0;
+
+// Create variables that hold references to the places in the HTML where we want to display things.
+        var wins = document.getElementByClass("wins");
+        var losses = document.getElementByClass("losses");
+        var guessesLeft = document.getElementByClass("guessesLeft");
+        var guessesSoFar = document.getElementByClass("guessesSoFar");
+
+// select random letter via for loop and randomMath function ???
+
+for (var aphabet = 0; j < arrayLength; alphabet++) {
+
+	// Randomly chooses a choice from the options array. This is the Computer's guess. EDIT BELOW
+
+	          var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+}
+
+// function to convert to lowercase
+
+var RANDOMMATHLower = RANDOMMATHGUESS.toLowerCase();
+
+// This function is run whenever the user presses a key.
+        document.onkeyup = function(event) {
+
+          // Determines which key was pressed.
+          var userGuess = event.key;
+
+// Created a prompt to get the user's quess.
+        var userGuess = prompt("Which letter am I thinking of?");
+
+        // Converts the user's answer to lowercase.
+        var userGuessLower = userGuess.toLowerCase();
+
+// if user selects matching letter assign a win to wins class (do while loop?), then reset game with initialize function
+
+if (alphabet.indexOf(userGuessLower) == RANDOMMATH) {
+          ASSIGN A WIN;
+        }
+
+or
+
+
+
+/* The Do/While Loop USE THIS???
+The do/while loop is a variant of the while loop. This loop will execute the code block once, before checking if the condition is true, then it will repeat the loop as long as the condition is true.
+
+Syntax
+do {
+  code block to be executed
+}
+while (condition);
+Example
+The example below uses a do/while loop. The loop will always be executed at least once, even if the condition is false, because the code block is executed before the condition is tested:
+
+Example
+do {
+  text += "The number is " + i;
+  i++;
+}
+while (i < 10);
+
+Do not forget to increase the variable used in the condition, otherwise the loop will never end!
+*/
+
+// else subtract one from guesses left class length,
+
+else {
+          SUBTRACT(#guessesLeft LENGTH)
+        }
+
+// print the letter the user selected with document.Write
+
+// when guess left in guesses left class length equal zero (do while loop?) assign a loss (wins and losses conditions), and reset game with initialize function
